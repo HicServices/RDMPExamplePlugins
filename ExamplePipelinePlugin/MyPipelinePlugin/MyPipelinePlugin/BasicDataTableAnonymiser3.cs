eddc9dc6d42c4e296a3f945f2e5e4ebfe87c6adf
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-05 11:12:37 +0100

    RDMPDEV-1428 BasicDataTableAnonymiser 2 and 3 implementations

---
+++ ExamplePipelinePlugin/MyPipelinePlugin/MyPipelinePlugin/BasicDataTableAnonymiser3.cs
@@ -0,0 +1,88 @@
+﻿using Rdmp.Core.Curation.Data;
+using Rdmp.Core.DataFlowPipeline;
+using ReusableLibraryCode.Checks;
+using ReusableLibraryCode.DataAccess;
+using ReusableLibraryCode.Progress;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MyPipelinePlugin
+{
+    public class BasicDataTableAnonymiser3: IPluginDataFlowComponent<DataTable>
+    {
+        [DemandsInitialization("Table containing a single column which must have a list of names to redact from columns", mandatory:true)]
+        public TableInfo NamesTable { get; set; }
+
+        [DemandsInitialization("Columns matching this regex pattern will be skipped")]
+        public Regex ColumnsNotToEvaluate { get; set; }
+
+        private string[] _commonNames;
+        
+        public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener,GracefulCancellationToken cancellationToken)
+        {
+            if (_commonNames == null)
+            {
+                //discover the table
+                var tableDiscovered = NamesTable.Discover(DataAccessContext.DataLoad);
+
+                //make sure it exists
+                if(!tableDiscovered.Exists())
+                    throw new NotSupportedException("TableInfo '" + tableDiscovered + "' does not exist!");
+                
+                //Download all the data
+                var dataTable = tableDiscovered.GetDataTable();
+
+                //Make sure it has the correct expected schema (i.e. 1 column)
+                if(dataTable.Columns.Count != 1)
+                    throw new NotSupportedException("Expected a single column in DataTable '" + tableDiscovered +"'");
+
+                //turn it into an array (throwing out any nulls)
+                _commonNames = dataTable.Rows.Cast<DataRow>().Select(r => r[0] as string).Where(s=>!string.IsNullOrWhiteSpace(s)).ToArray();
+            }
+
+            //Go through each row in the table
+            foreach (DataRow row in toProcess.Rows)
+            {
+                //for each cell in current row
+                foreach (DataColumn col in toProcess.Columns)
+                {
+                    //if it's not a column we are skipping
+                    if(ColumnsNotToEvaluate != null && ColumnsNotToEvaluate.IsMatch(col.ColumnName))
+                        continue;
+                    
+                    //if it is a string
+                    var stringValue = row[col] as string;
+
+                    if(stringValue != null)
+                    {
+                        //replace any common names with REDACTED
+                        foreach (var name in _commonNames)
+                            stringValue =  Regex.Replace(stringValue, name, "REDACTED",RegexOptions.IgnoreCase);
+
+                        row[col] = stringValue;
+                    }
+                }
+            }
+
+            return toProcess;
+        }
+
+        public void Dispose(IDataLoadEventListener listener, Exception pipelineFailureExceptionIfAny)
+        {
+            
+        }
+
+        public void Abort(IDataLoadEventListener listener)
+        {
+            
+        }
+
+        public void Check(ICheckNotifier notifier)
+        {
+            
+        }
+    }
+
+}
