a93f94b27cf9d4b120cfa0dfd21e6f4fa40078b6
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-05 09:23:12 +0100

    RDMPDEV-1428 BasicDataTableAnonymiser1 implementation

---
+++ ExamplePipelinePlugin/MyPipelinePlugin/MyPipelinePlugin/BasicDataTableAnonymiser1.cs
@@ -0,0 +1,58 @@
+﻿using Rdmp.Core.DataFlowPipeline;
+using ReusableLibraryCode.Checks;
+using ReusableLibraryCode.Progress;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace MyPipelinePlugin
+{
+    class BasicDataTableAnonymiser1 : IPluginDataFlowComponent<DataTable>
+    {
+        public void Abort(IDataLoadEventListener listener)
+        {
+            
+        }
+
+        public void Check(ICheckNotifier notifier)
+        {
+            
+        }
+
+        public void Dispose(IDataLoadEventListener listener, Exception pipelineFailureExceptionIfAny)
+        {
+            
+        }
+
+        public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener, GracefulCancellationToken cancellationToken)
+        {
+            //Go through each row in the table
+		    foreach (DataRow row in toProcess.Rows)
+		    {
+			    //for each cell in current row
+			    for (int i = 0; i < row.ItemArray.Length; i++)
+			    {
+				    //if it is a string
+				    var stringValue = row[i] as string;
+
+				    if(stringValue != null)
+				    {
+					    //replace any common names with REDACTED
+					    foreach (var name in CommonNames)
+						    stringValue =  Regex.Replace(stringValue, name, "REDACTED",RegexOptions.IgnoreCase);
+
+					    row[i] = stringValue;
+				    }
+			    }
+            }
+
+		    return toProcess;
+        }
+
+        string[] CommonNames = new string[]
+        { 
+            "Dave","Frank","Bob","Pete","Daisy","Marley","Lucy","Tabitha"
+        };
+
+    }
+}
---
+++ ExamplePipelinePlugin/MyPipelinePlugin/Plugin/netcoreapp2.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2d09fc4a-3a82-4181-a3d2-cd0e2f4a7f01")]
